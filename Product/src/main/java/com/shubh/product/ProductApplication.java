6b5fd330cea3fafcf08dbfab62da602d38be59b4
shubhmehrotra <shubhmehrotra@gmaill.com>
2024-05-23 00:36:13 +0530

    Refactored

--- Product/src/main/java/com/shubh/product/ProductApplication.java
+++ Product/src/main/java/com/shubh/product/ProductApplication.java
@@ -1,14 +1,14 @@
-package com.Shubh.JPARealtionship;
+package com.shubh.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-public class Application {
+public class ProductApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(ProductApplication.class, args);
 	}
 
 }
