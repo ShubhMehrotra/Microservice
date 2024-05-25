aa8a21284e35b17e8b66e267767f64636263a6c7
shubhmehrotra <shubhmehrotra@gmaill.com>
2024-05-26 01:49:52 +0530

    API gateway implemented for Customer & Product

--- ApiGateway/src/main/java/com/shubh/gateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/shubh/gateway/ApiGatewayApplication.java
@@ -2,6 +2,9 @@ package com.shubh.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
--- Product/src/main/java/com/shubh/product/ProductApplication.java
+++ Product/src/main/java/com/shubh/product/ProductApplication.java
@@ -3,7 +3,9 @@ package com.shubh.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProductApplication {
 
